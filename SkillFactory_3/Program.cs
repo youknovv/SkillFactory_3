f6eb3cd59306cd1fb0a4599031d85a21f6442017
youknovv <youknovv@mail.ru>
2023-12-17 05:13:52 +0700

    Добавьте файлы проекта.

---
+++ SkillFactory_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        const string MyName = "Daniil";
+
+        Console.WriteLine(MyName);
+
+        Console.WriteLine("\t Привет, мир!");
+        Console.WriteLine("\t Мне 20 лет");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0040");
+        Console.ReadKey();
+
+    }
+}
