67396931420947f937d54b737fb60d3f01ce787b
youknovv <youknovv@mail.ru>
2023-12-17 05:21:59 +0700

    Задание 3.3.2

--- SkillFactory_3/Program.cs
+++ SkillFactory_3/Program.cs
@@ -5,13 +5,15 @@ class MainClass
     public static void Main(string[] args)
     {
         const string MyName = "Daniil";
+        byte MyAge = 20;
+        bool Pet = true;
+        double ShoeSize = 44.5;
 
-        Console.WriteLine(MyName);
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("My age is " + MyAge);
+        Console.WriteLine("Do I have a pet? " + Pet);
+        Console.WriteLine("My shoe size is " + ShoeSize);
 
-        Console.WriteLine("\t Привет, мир!");
-        Console.WriteLine("\t Мне 20 лет");
-        Console.WriteLine("\t My name is \n {0}", MyName);
-        Console.WriteLine("\u0040");
         Console.ReadKey();
 
     }
