0ea2f95d5c7d13dd2ee9964b00a7343da0436f62
youknovv <youknovv@mail.ru>
2024-01-06 18:46:20 +0700

    4.1.17

--- SkillFactory_3/Program.cs
+++ SkillFactory_3/Program.cs
@@ -4,16 +4,31 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.Write("Enter your name: ");
-        var name = Console.ReadLine();
-        Console.Write("Enter your age: ");
-        var age = Convert.ToInt32(Console.ReadLine());
-        Console.Write($"Your name is {name} and age is {age} \n");
-        Console.Write("Enter your birthdate: ");
-        var birthdate= Console.ReadLine();
-        Console.Write($"Your birthdate is {birthdate}");
+        Console.WriteLine("Напишите свой любимый цвет с маленькой буквы");
 
-        Console.ReadKey();
+        var color = Console.ReadLine();
+
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is red");
+        }
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine(" Your color is green");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan");
+        }
     }
 
 }
