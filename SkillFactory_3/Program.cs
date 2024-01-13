71623ccaa7e6a6be490ff0c28447895089bd076f
Felmin Encode <youknovv@mail.ru>
2024-01-13 20:40:54 +0700

    Add "case"

--- SkillFactory_3/Program.cs
+++ SkillFactory_3/Program.cs
@@ -6,29 +6,39 @@ class MainClass
     {
         Console.WriteLine("Напишите свой любимый цвет с маленькой буквы");
 
-        var color = Console.ReadLine();
-
-        if (color == "red")
+        for (int i = 0; i < 5; i++)
         {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Iteration {0}", i);
+            switch (Console.ReadLine())
+            {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.WriteLine("Your color is red");
-        }
-        else if (color == "green")
-        {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is red!");
+                break;
 
-            Console.WriteLine(" Your color is green");
-        }
-        else
-        {
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Black;
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.WriteLine("Your color is cyan");
+                Console.WriteLine("Your color is green!");
+                break;
+
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
+            }
         }
     }
-
 }
