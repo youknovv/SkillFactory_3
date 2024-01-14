5a338d5f905950c5f8553635445aa8aaa1b70644
Felmin Encode <youknovv@mail.ru>
2024-01-14 23:18:50 +0700

    4.3.7

--- SkillFactory_3/Program.cs
+++ SkillFactory_3/Program.cs
@@ -40,5 +40,13 @@ class MainClass
                 break;
             }
         }
+
+        Console.WriteLine("Напишите ваше имя: ");
+        var name = Console.ReadLine();
+
+        for (int i = name.Length - 1; i >= 0; i--)
+        {
+            Console.Write(name[i] + " ");
+        }
     }
 }
