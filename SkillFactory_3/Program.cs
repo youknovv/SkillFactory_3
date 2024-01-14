8f36b3027833671f9a1787c3fb823e35f147c919
Felmin Encode <youknovv@mail.ru>
2024-01-14 23:35:41 +0700

    4.3.11

--- SkillFactory_3/Program.cs
+++ SkillFactory_3/Program.cs
@@ -45,8 +45,19 @@ class MainClass
         var name = Console.ReadLine();
 
         for (int i = name.Length - 1; i >= 0; i--)
-        {
             Console.Write(name[i] + " ");
+
+        Console.WriteLine();
+
+
+        int[,] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+
+        for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+        {
+            for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                Console.Write(array[k, i] + " ");
+
+            Console.WriteLine();
         }
     }
 }
